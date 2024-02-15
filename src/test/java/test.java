12bdeb365704cd57129c6c45934f1ff73bf0ec1c
Anton4321G <anton4321garanin@gmail.com>
2024-02-14 20:01:33 -0500

    ewerew

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("i on batch 18 student");
+        System.out.println("new commit by new user");
     }
 }
